5bd84aa1ae6ecd86600a8a1fcdbcc4e6035479fb
matviizakrevskyi <matvii.zakrevskyi@microsoft.wsei.edu.pl>
2022-03-11 21:44:19 +0100

    Update Program.cs

--- lab-1/Program.cs
+++ lab-1/Program.cs
@@ -10,14 +10,31 @@ namespace lab_1
             //Object obj = person;
             //Object obj1;
 
-
-            Money money = new Money(128, Currency.PLN);
+            Money money1 = Money.ParseValue("12,45", Currency.PLN);
+            Console.WriteLine(money1.ToString());
+            Money money = Money.OfWithExeption(128, Currency.PLN);
             decimal cost = money;
-            double price = (double) money;
+            double price = (double)money1;
+            float pricefloat = (float)money;
+            Console.WriteLine(cost + ", " + price + ", " + pricefloat);
             string str = (string)money;
             Console.WriteLine(str);
             Console.WriteLine(money.ToString());
 
+            Console.WriteLine();
+            Tank tank1 = new Tank(100);
+            tank1.refuel(48);
+            tank1.Show();
+            tank1.consume(2);
+            tank1.Show();
+            Tank tank2 = new Tank(250);
+            tank2.refuel(120);
+            tank1.refuel(tank2, 100);
+            tank1.Show();
+            tank1.refuel(tank2, 20);
+            tank1.Show();
+
+            Console.WriteLine();
             Money[] prices =
             {
                 Money.Of(10, Currency.PLN),
@@ -29,14 +46,16 @@ namespace lab_1
                 Money.Of(71, Currency.PLN),
                 Money.Of(9, Currency.EUR)
             };
-            Array.Sort(prices);
-            foreach (var p in prices)
-            {
-                Console.WriteLine(p);
-            }
+            //Array.Sort(prices);
+            //foreach (var p in prices)
+            //{
+            //    Console.WriteLine(p);
+            //}
+
         }
     }
 
+    //interfaces
     interface Equal
     {
         public bool Equals(Object obj);
@@ -46,6 +65,7 @@ namespace lab_1
         public int CompareTo(Money other);
     }
 
+    //Person
     public class Person : Equal
     {
         private string name;
@@ -101,18 +121,19 @@ namespace lab_1
         }
     }
 
-
+    //Money
     public enum Currency
     {
         PLN = 1,
         USD = 2,
         EUR = 3
     }
-
     public class Money : IComparable
     {
-        
+
         private decimal _value;
+        private Currency _currency;
+
         public decimal Value
         {
             get
@@ -124,7 +145,6 @@ namespace lab_1
                 _value = value;
             }
         }
-        private Currency _currency;
         public Currency Currency
         {
             get
@@ -136,21 +156,45 @@ namespace lab_1
                 _currency = value;
             }
         }
-        public Money(decimal value, Currency currency)
+
+        //constructor
+        private Money(decimal value, Currency currency)
         {
             Value = value;
             Currency = currency;
         }
 
+        //Of
         public static Money? Of(decimal value, Currency currency)
         {
             return value < 0 ? null : new Money(value, currency);
         }
 
+        public static Money OfWithExeption(decimal value, Currency currency)
+        {
+            if (value >= 0)
+            {
+                return value < 0 ? null : new Money(value, currency);
+            }
+            else
+            {
+                throw new ArgumentException("Argument exception!!!");
+            }
+        }
 
+        public static Money ParseValue(string valueStr, Currency currency)
+        {
+            return new Money(decimal.Parse(valueStr), currency);
+        }
+
+        //operators
         public static Money operator *(Money money, decimal factor)
         {
             return new Money(money.Value * factor, money.Currency);
+        }
+        public static decimal operator +(Money a, Money b)
+        {
+            return a.Value + b.Value;
         }
         public static bool operator <(Money a, Money b)
         {
@@ -160,7 +204,7 @@ namespace lab_1
         public static bool operator >(Money a, Money b)
         {
             IsSameCurriences(a, b);
-            return a.Value > b.Value; 
+            return a.Value > b.Value;
         }
         private static void IsSameCurriences(Money a, Money b)
         {
@@ -178,6 +222,11 @@ namespace lab_1
         public static explicit operator double(Money money)
         {
             return (double)money.Value;
+        }
+
+        public static explicit operator float(Money money)
+        {
+            return (float)money.Value;
         }
 
         public static explicit operator string(Money money)
@@ -203,6 +252,78 @@ namespace lab_1
             {
                 return currencyCon;
             }
+        }
+    }
+
+    //Tank
+    public class Tank
+    {
+        public readonly int Capacity;
+        private int _level;
+        public int Level
+        {
+            get
+            {
+                return _level;
+            }
+            private set
+            {
+                if (value < 0 || value > Capacity)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+                _level = value;
+            }
+        }
+        public Tank(int capacity)
+        {
+            Capacity = capacity;
+        }
+
+        public void refuel(int amount)
+        {
+            if (amount < 0)
+            {
+                throw new ArgumentException("Argument cant be non positive!");
+            }
+            if (_level + amount > Capacity)
+            {
+                throw new ArgumentException("Argument is to large!");
+            }
+            _level += amount;
+        }
+
+        public void consume(int amount)
+        {
+            if (amount < 0)
+            {
+                throw new ArgumentException("Argument cant be non positive!");
+            }
+            if (amount > _level)
+            {
+                throw new ArgumentException("Argument is to large!");
+            }
+            _level += amount;
+        }
+
+        public bool refuel(Tank sourceTank, int amount)
+        {
+            if (amount < 0)
+                return false;
+            if (amount > sourceTank._level)
+                return false;
+            if (amount + this._level > this.Capacity)
+                return false;
+
+            sourceTank._level -= amount;
+            this._level += amount;
+            return true;
+        }
+
+
+        public void Show()
+        {
+            Console.WriteLine(this.Level);
         }
     }
 }
