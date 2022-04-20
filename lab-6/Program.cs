70fb4c0b04f9a7099981686582710ae6dbaf7a6a
matviizakrevskyi <matvii.zakrevskyi@microsoft.wsei.edu.pl>
2022-04-20 13:11:44 +0200

    lab-7

--- lab-6/Program.cs
+++ lab-6/Program.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 
 namespace lab_6
 {
-    class Student
+    class Student : IComparable<Student>
     {
         public string Name { get; set; }
         public int Ects { get; set; }
+
+        public int CompareTo(Student other)
+        {
+            if (Name.CompareTo(other.Name) == 0)
+            {
+                return Ects.CompareTo(other.Ects);
+            }
+            return Name.CompareTo(other.Name);
+        }
 
         public override bool Equals(object obj)
         {
@@ -14,6 +23,17 @@ namespace lab_6
             return obj is Student student &&
                    Name == student.Name &&
                    Ects == student.Ects;
+        }
+
+        public override int GetHashCode()
+        {
+            Console.WriteLine("Students HashCode");
+            return HashCode.Combine(Name, Ects);
+        }
+
+        public override string ToString()
+        {
+            return $"Name = {Name}, Ects = {Ects}";
         }
     }
 
@@ -31,7 +51,7 @@ namespace lab_6
             }
             Console.WriteLine(names.Contains("Adam"));
 
-            //
+            //--
             Console.WriteLine();
 
             ICollection<Student> students = new List<Student>();
@@ -44,6 +64,10 @@ namespace lab_6
                 Console.WriteLine(student.Name + ", " + student.Ects);
             }
             Console.WriteLine(students.Contains(new Student { Name = "Karol", Ects = 8 }));
+
+            //--
+            Console.WriteLine();
+
             List<Student> list = (List<Student>)students;
             Console.WriteLine(list[0]);
             list.Insert(1, new Student { Name = "Robert", Ects = 12 });
@@ -53,9 +77,77 @@ namespace lab_6
             }
             int index = list.IndexOf(new Student { Name = "Adam", Ects = 1 });
             Console.WriteLine("Karol ma pozycje " + index);
-            list.RemoveAt(index);
+            //list.RemoveAt(index);
 
+            //--
+            Console.WriteLine("---------------------SET---------------------");
             ISet<string> set = new HashSet<string>();
+            set.Add("Ewa");
+            set.Add("Karol");
+            set.Add("Adam");
+            set.Add("Adam");
+            foreach (var item in set)
+            {
+                Console.WriteLine(item);
+            }
+
+            //--
+            Console.WriteLine();
+
+            ISet<Student> studentsGroup = new HashSet<Student>();
+            studentsGroup.Add(new Student { Name = "Ewa", Ects = 4 });
+            studentsGroup.Add(new Student { Name = "Adam", Ects = 12 });
+            studentsGroup.Add(new Student { Name = "Karol", Ects = 8 });
+            studentsGroup.Add(new Student { Name = "Ewa", Ects = 64 });
+            foreach (var item in studentsGroup)
+            {
+                Console.WriteLine(item.Name + ", " + item.Ects);
+            }
+            studentsGroup.Contains(new Student { Name = "Ewa", Ects = 4 });
+            //studentsGroup.Remove(new Student { Name = "Ewa", Ects = 4 });
+
+            //--
+            Console.WriteLine();
+
+
+            studentsGroup = new SortedSet<Student>(studentsGroup);
+            foreach (var item in studentsGroup)
+            {
+                Console.WriteLine(item.Name + ", " + item.Ects);
+            }
+
+            studentsGroup.Contains(new Student { Name = "Ewa", Ects = 4 });
+
+            //--
+            Console.WriteLine();
+
+            Dictionary<Student, string> phoneBook = new Dictionary<Student, string>();
+            phoneBook[list[0]] = "123678456";
+            phoneBook[list[1]] = "543647234";
+            phoneBook[list[2]] = "123677345";
+            Console.WriteLine(phoneBook.Keys);
+            if (phoneBook.ContainsKey(new Student { Name = "Adam", Ects = 1 }))
+            {
+                Console.WriteLine("Jest telefon");
+            }
+            else
+            {
+                Console.WriteLine("Brak telefonu");
+            }
+
+            foreach (var item in phoneBook)
+            {
+                Console.WriteLine(item.Key + ", Phone = " + item.Value);
+            }
+
+            //--
+            Console.WriteLine();
+
+            string[] arr = { "adam", "ewa", "karol", "ewa", "ania", "karol", "adam", "adam", "ewa" };
+            //oblicz ile razy wystepuje kazde z imion w tablicy arr
+            Dictionary<string, int> counters = new Dictionary<string, int>();
+            
+
         }
     }
 }
---
+++ lab-7/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace lab_7
+{
+    delegate double Operation(double a, double b);
+    delegate double Power(double a, double b);
+    class Program
+    {
+        static double Addition(double x, double y)
+        {
+            return x + y;
+        }
+
+        static double Mul(double x, double y)
+        {
+            return x * y;
+        }
+
+        static void Main(string[] args)
+        {
+            Operation add = Addition;
+            double result = add.Invoke(3, 5);   //add.Invoke(x, y)  or  add(x, y)
+            Console.WriteLine(result);
+            add = Mul;
+            Console.WriteLine(add.Invoke(3, 5));
+
+            Func<double, double, double> Operator = Addition;
+            Func<double, double, double> Div = delegate (double x, double y)
+            {
+                return x / y;
+            };
+
+            Func<int> RandomInt = delegate ()
+            {
+                return new Random().Next(100);
+            };
+            Console.WriteLine(RandomInt.Invoke());
+
+            Predicate<int> InRangeFrom0To100 = delegate (int value)
+            {
+                return value >= 0 && value <= 100;
+            };
+            Console.WriteLine(InRangeFrom0To100(32));
+
+            Func<int, int, int, bool> InRange = delegate (int value, int min, int max)
+            {
+                return value >= min && value <= max;
+            };
+
+            Action<string> Print = delegate (string s)
+            {
+                Console.WriteLine(s);
+            };
+
+            Print("abc");
+
+            Operation AddLambda = (a, b) => a + b;
+            Func<double, double, double> DivLambda = (x, y) =>
+            {
+                if (y != 0)
+                {
+                    return x / y;
+                }
+                else
+                {
+                    throw new Exception("y is zero!");
+                }
+            };
+
+            Predicate<string> ThreeCharacters = (s) => s.Length == 3;
+
+            Action<string> PrintUpperLambda = (s) => Console.WriteLine(s.ToUpper());
+        }
+    }
+}
